4126a177f327497e577779946bec79fdcbe56a3b
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-08-22 21:32:27 +0500

    UsageRecordFactory ArrayList named as ticketArrayList added

--- src/bcccp/tickets/season/UsageRecordFactory.java
+++ src/bcccp/tickets/season/UsageRecordFactory.java
@@ -1,6 +1,10 @@
 package bcccp.tickets.season;
 
+import java.util.ArrayList;
+
 public class UsageRecordFactory implements IUsageRecordFactory {
+
+	ArrayList<SeasonTicket> ticketArrayList;
 
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
