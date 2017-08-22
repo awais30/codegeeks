89ac6903bd71c900934f24e0af9017147fc7eaa2
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-08-22 19:33:56 +0500

    Usage Record stateDateTime variable initialized

--- src/bcccp/tickets/season/UsageRecord.java
+++ src/bcccp/tickets/season/UsageRecord.java
@@ -9,10 +9,10 @@ public class UsageRecord implements IUsageRecord {
 
 	public UsageRecord(){
 		ticketId = "11111"; //default value
+        startDateTime = -1;
 	}
 	
 	public UsageRecord(String ticketId, long startDateTime) {
-		//TODO Implement constructor
 
 	}
 
