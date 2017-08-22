a9fd5ecbaf32ce5e02d671b87945168f8cb6a8e4
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-08-22 19:10:36 +0500

    Initializing the class variables for Season Ticket Commenting done properly

--- src/bcccp/tickets/season/SeasonTicket.java
+++ src/bcccp/tickets/season/SeasonTicket.java
@@ -18,9 +18,7 @@ public class SeasonTicket implements ISeasonTicket {
 			             String carparkId, 
 			             long startValidPeriod,
 			             long endValidPeriod) {
-							 
-							 // Class Constructor variables declarication
-		//TDO Implement constructor
+
 		//Initializing the class variables
 		this.ticketId = "11111"; // 11111 is the default ticket id at the start of the system.
 		this.carparkId = "22222"; // 22222 is the defaul carpar id at the start of the system.
