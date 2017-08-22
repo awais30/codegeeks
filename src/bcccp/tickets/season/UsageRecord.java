541a59087cf12fbd19c0731aaa0b7e04af9abf6e
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-08-22 19:48:58 +0500

    Usage Record Constructor ticketId variable been initiaized using this keyword

--- src/bcccp/tickets/season/UsageRecord.java
+++ src/bcccp/tickets/season/UsageRecord.java
@@ -14,7 +14,7 @@ public class UsageRecord implements IUsageRecord {
 	}
 	
 	public UsageRecord(String ticketId, long startDateTime) {
-		ticketId = ticketId; //value been set
+		this.ticketId = ticketId; //value been set
 		startDateTime = startDateTime; //value been set
 	}
 
