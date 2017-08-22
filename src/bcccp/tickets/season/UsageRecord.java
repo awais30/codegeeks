d8279fdac6129ea7582f0070bbf6041ab216666e
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-08-22 21:12:08 +0500

    Usage Record Override Method: getSeasonTicketId() comment added

--- src/bcccp/tickets/season/UsageRecord.java
+++ src/bcccp/tickets/season/UsageRecord.java
@@ -35,7 +35,7 @@ public class UsageRecord implements IUsageRecord {
 
 	@Override
 	public String getSeasonTicketId() {
-		return this.ticketId;
+		return this.ticketId; // returns the value of the ticketId variable
 	}
 	
 }
