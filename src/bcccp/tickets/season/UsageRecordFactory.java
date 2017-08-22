e32c041821b01df230a865bf3f5a21b05dc3c53b
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-08-22 21:34:55 +0500

    UsageRecordFactory Using this keyword ArrayList : ticketArrayList been initalized to store all the session tickets

--- src/bcccp/tickets/season/UsageRecordFactory.java
+++ src/bcccp/tickets/season/UsageRecordFactory.java
@@ -7,7 +7,7 @@ public class UsageRecordFactory implements IUsageRecordFactory {
 	ArrayList<SeasonTicket> ticketArrayList; //to keep the list of all the session tickets been issued
 
 	public UsageRecordFactory(){
-		ticketArrayList = new ArrayList<SeasonTicket>();
+		this.ticketArrayList = new ArrayList<SeasonTicket>();
 	}
 
 	@Override
