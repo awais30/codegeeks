551d86799ac2eab96f8e44c859113b5f7bcb862d
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-08-22 18:47:04 +0500

    Initializing the class variables for Season Ticket 22222 is the default carpark id at the start of the system.

--- src/bcccp/tickets/season/SeasonTicket.java
+++ src/bcccp/tickets/season/SeasonTicket.java
@@ -22,7 +22,8 @@ public class SeasonTicket implements ISeasonTicket {
 							 // Class Constructor variables declarication
 		//TDO Implement constructor
 		//Initializing the class variables
-		this.ticketId = "111111"; // 11111 is the default ticket id at the start of the system.
+		this.ticketId = "11111"; // 11111 is the default ticket id at the start of the system.
+		this.carparkId = "22222"; // 22222 is the defaul carpar id at the start of the system.
 	}
 
 	@Override
