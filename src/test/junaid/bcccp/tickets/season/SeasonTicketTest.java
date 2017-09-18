0bedb428e96723ac899fe4a5008ec126a18f1b42
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-09-18 14:50:58 +0500

    SeasonTicketTest Class added for the Unit Testing

---
+++ src/test/junaid/bcccp/tickets/season/SeasonTicketTest.java
@@ -0,0 +1,13 @@
+package bcccp.tickets.season;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by jaju on 9/18/17.
+ */
+class SeasonTicketTest {
+
+
+}
