24fe96debc1c0495857f9e37b0f5205774e9029e
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-09-19 20:47:34 +0500

    Integration Test 2

--- src/test/junaid/bcccp/tickets/season/SeasonTicketDAOTest.java
+++ src/test/junaid/bcccp/tickets/season/SeasonTicketDAOTest.java
@@ -2,6 +2,8 @@ package bcccp.tickets.season;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -28,5 +30,59 @@ class SeasonTicketDAOTest {
     @Test
     void registerTicket(){
         //Creating Object
+        ISeasonTicket iSeasonTicket = new ISeasonTicket() {
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public String getCarparkId() {
+                return null;
+            }
+
+            @Override
+            public long getStartValidPeriod() {
+                return 0;
+            }
+
+            @Override
+            public long getEndValidPeriod() {
+                return 0;
+            }
+
+            @Override
+            public boolean inUse() {
+                return false;
+            }
+
+            @Override
+            public void recordUsage(IUsageRecord record) {
+
+            }
+
+            @Override
+            public IUsageRecord getCurrentUsageRecord() {
+                return null;
+            }
+
+            @Override
+            public void endUsage(long dateTime) {
+
+            }
+
+            @Override
+            public List<IUsageRecord> getUsageRecords() {
+                return null;
+            }
+        };
+        IUsageRecordFactory factory = new IUsageRecordFactory() {
+            @Override
+            public IUsageRecord make(String ticketId, long startDateTime) {
+                return null;
+            }
+        };
+        SeasonTicketDAO seasonTicketDAO = new SeasonTicketDAO(factory);
+        seasonTicketDAO.registerTicket(iSeasonTicket);
     }
 }
