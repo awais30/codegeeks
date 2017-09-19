328d370f285956371c5b978eeaa77a164f52ed19
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-09-19 16:31:40 +0500

    Unit Test Case 10

--- src/test/junaid/bcccp/tickets/season/UsageRecordTest.java
+++ src/test/junaid/bcccp/tickets/season/UsageRecordTest.java
@@ -25,5 +25,6 @@ class UsageRecordTest {
         UsageRecord usageRecord = new UsageRecord("NobalPark1111", 1111);
         //Creating time as long variable
         long time = 0;
+        //Using assertEqual method to check if the return value is good or not
     }
 }
