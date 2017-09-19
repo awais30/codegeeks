ec6b3cf261427474b78cb0085153cfe0cbb11b7b
awais30 <muhammad.awais270@gmail.com>
2017-09-19 15:46:35 +1000

    Exit Time calculation
    Calculate the Exit time user

--- src/bcccp/tickets/adhoc/AdhocTicket.java
+++ src/bcccp/tickets/adhoc/AdhocTicket.java
@@ -132,7 +132,25 @@ public class AdhocTicket implements IAdhocTicket {
 
 	@Override
 	public void exit(long dateTime) {
-		// TODO Auto-generated method stub
+		//DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		private static final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+         private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+
+        Date date = new Date();
+        System.out.println(sdf.format(date));
+
+        Calendar cal = Calendar.getInstance();
+        System.out.println(sdf.format(cal.getTime()));
+
+       /* LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+
+        LocalDate localDate = LocalDate.now();
+        System.out.println(DateTimeFormatter.ofPattern("yyy/MM/dd").format(localDate));
+
+		// TODO Auto-generated method stub */
+		// TODO Auto-generated method stub */
 		
 	}
 
@@ -150,6 +168,7 @@ public class AdhocTicket implements IAdhocTicket {
 	@Override
 	public boolean hasExited() {
 		// TODO Auto-generated method stub
+		if()
 		return false;
 	}
 
