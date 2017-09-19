a44c725ae93bc3a13f711d4dd17883f9335d4f66
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-09-19 16:13:47 +0500

    UsageRecord class been updated according to the code given in the assignment 3 resources

--- src/test/junaid/bcccp/tickets/season/UsageRecordTest.java
+++ src/test/junaid/bcccp/tickets/season/UsageRecordTest.java
@@ -14,5 +14,6 @@ class UsageRecordTest {
         //Creating object
         UsageRecord usageRecord = new UsageRecord("NobalPark1111", 1111);
         //Creating time as long variable
+        long time;
     }
 }
