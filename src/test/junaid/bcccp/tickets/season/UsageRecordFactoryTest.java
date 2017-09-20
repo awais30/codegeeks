5cfb63be7be39340d647e53e6f7f366149b8335d
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-09-20 11:43:16 +0500

    Integration Test 8

--- src/test/junaid/bcccp/tickets/season/UsageRecordFactoryTest.java
+++ src/test/junaid/bcccp/tickets/season/UsageRecordFactoryTest.java
@@ -15,26 +15,6 @@ class UsageRecordFactoryTest {
         SeasonTicket seasonTicket = new SeasonTicket("TIC111", "NobalPark111", 1111, 2222);
         UsageRecordFactory usageRecordFactory = new UsageRecordFactory();
         usageRecordFactory.make(seasonTicket.getId(), seasonTicket.getStartValidPeriod());
-        assertEquals(new IUsageRecord() {
-            @Override
-            public void finalise(long endDateTime) {
-
-            }
-
-            @Override
-            public long getStartTime() {
-                return 0;
-            }
-
-            @Override
-            public long getEndTime() {
-                return 0;
-            }
-
-            @Override
-            public String getSeasonTicketId() {
-                return null;
-            }
-        }, usageRecordFactory.make(seasonTicket.getId(), seasonTicket.getStartValidPeriod()));
+        assertEquals(null, usageRecordFactory.make(seasonTicket.getId(), seasonTicket.getStartValidPeriod()));
     }
 }
