d680235aad030a255c403b3003cc9e9b21e98868
awais30 <muhammad.awais270@gmail.com>
2017-09-20 21:53:27 +1000

    initilizing ticket id

--- src/bcccp/tickets/season/SeasonTicket.java
+++ src/bcccp/tickets/season/SeasonTicket.java
@@ -21,8 +21,8 @@ public class SeasonTicket implements ISeasonTicket {
 							 
 
 		//Initializing the class variables
-		this.ticketId = "4444"; // 11111 is the default ticket id at the start of the system.
-		this.carparkId = "22222"; // 22222 is the defaul carpar id at the start of the system.
+		this.ticketId = "4444"; // 4444 is the default ticket id at the start of the system.
+		this.carparkId = "55555"; // 5555 is the defaul carpar id at the start of the system.
 		this.startValidPeriod = -1; //-1 is the default valid period
         this.endValidPeriod = -1; // -1 is the default valid period
 	}
