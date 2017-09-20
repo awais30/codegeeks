d16117862ec8c466a34a550b4d63678a59776805
jaypatel1427 <pateljay672467@gmail.com>
2017-09-20 23:24:29 +1000

    Add files via upload

---
+++ src/bcccp/ICarparkTest.java
@@ -0,0 +1,66 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ICarparkTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testRegister() {
+		 System.out.println("Current test is " + getRegister());
+		 System.out.println("working status");// TODO
+	}
+
+	private String getRegister() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Test
+	public final void testDeregister() {
+		  System.out.println("Current test is " + getDeregister());
+ // TODO
+	}
+
+	private String getDeregister() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Test
+	public final void testGetName() {
+		
+	
+		  System.out.println("Current test is " + getName());// TODO
+	}
+
+	private String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Test
+	public final void testIsFull() {
+		 System.out.println("Current test is " + getIsfull()); // TODO
+	}
+
+	private String getIsfull() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
